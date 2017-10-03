20354d57c41e2a5c5b94b8475d4020f990a4f729
aitap <krylov.r00t@gmail.com>
2017-10-03 23:51:43 +0300

    idea for the library interface

---
+++ stellarnet_ep2.h
@@ -0,0 +1,8 @@
+char ep2_parse(
+	/* in */ const char * /* filename */,
+	/* in, opt */ float * /* wavelen_coefs */,
+	/* out */ size_t * /* num_spectra */,
+	/* out */ size_t * /* num_points */,
+	/* out */ float ** /* wavelengths */,
+	/* out */ float *** /* intensities */
+);
